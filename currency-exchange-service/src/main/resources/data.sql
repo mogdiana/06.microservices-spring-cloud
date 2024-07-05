30d43e23974b1964f5f658196d1880472b393820
dmoglan <mogdiana@gmail.com>
2024-07-05 11:26:06 +0300

    adding currency exchange, configuring jpa and data

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into currency_exchange (id,currency_from,currency_to,conversion_multiple,environment) values
+(10001,'USD','LEI',18,''),
+(10002,'EUR','LEI',19,''),
+(10003,'RON','LEI',4,'');
