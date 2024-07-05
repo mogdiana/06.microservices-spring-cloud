723b7d93890446458486d212eb05833fe514046c
dmoglan <mogdiana@gmail.com>
2024-07-05 12:45:57 +0300

    creating a service for currency conversion

---
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,24 @@
+package com.microservices.currencyconversionservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConversion {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+
+}
